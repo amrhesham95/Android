86148f5203a77dbfe06afd16de6a2a243a16caba
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2019-03-25 20:49:31 +0200

    VolleyApiDemo

---
+++ VolleyDemo/app/src/main/java/com/example/volleydemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.volleydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textViewID);
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        String url="https://www.google.com/";
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("Error");
+            }
+        });
+        requestQueue.add(stringRequest);
+
+    }
+}
