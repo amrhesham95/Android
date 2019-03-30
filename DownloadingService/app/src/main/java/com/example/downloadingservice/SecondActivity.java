a8c20d4c49a54a1228b84cf2558cf103c9999a02
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2019-03-30 18:19:08 +0200

    added shared data
    now i can use my application whenever i try to share a picture from gallery for an example

--- DownloadingService/app/src/main/java/com/example/downloadingservice/SecondActivity.java
+++ DownloadingService/app/src/main/java/com/example/downloadingservice/SecondActivity.java
@@ -1,27 +1,45 @@
 package com.example.downloadingservice;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+
 public class SecondActivity extends AppCompatActivity {
     ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        imageView=findViewById(R.id.imageView);
-        try {
-            FileInputStream fileInputStream= openFileInput(getIntent().getStringExtra("fileName"));
-            imageView.setImageBitmap(BitmapFactory.decodeStream(fileInputStream));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        imageView = findViewById(R.id.imageView);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
 
+        if (Intent.ACTION_SEND.equals(action) && type.startsWith("image/")) {
+            //Toast.makeText(this, intent.getParcelableExtra(Intent.EXTRA_STREAM).toString(), Toast.LENGTH_LONG).show();
+            Uri imageURI=(Uri)intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            imageView.setImageURI(imageURI);
+        } else {
+            try {
+
+                FileInputStream fileInputStream = openFileInput(getIntent().getStringExtra("fileName"));
+                imageView.setImageBitmap(BitmapFactory.decodeStream(fileInputStream));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
